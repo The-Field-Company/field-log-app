5fe787fc91dc13a8faf2c2ff3f1aaedf909ad0b5
Martin Muti <martinkilombe@gmail.com>
2026-02-25 09:41:03 +0300

    fix; failing test in test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,9 +3,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fieldlog_mobile/main.dart';
 
 void main() {
-  testWidgets('App renders entry screen', (WidgetTester tester) async {
+  testWidgets('App renders login screen', (WidgetTester tester) async {
     await tester.pumpWidget(const FieldLogApp(initialRoute: '/login'));
-    expect(find.text('FieldLog'), findsOneWidget);
-    expect(find.text('Load Form'), findsOneWidget);
+    expect(find.text('Scientific Field Data Collection'), findsOneWidget);
+    expect(find.text('Sign In'), findsOneWidget);
   });
 }
